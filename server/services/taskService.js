3facf47b94f1f66cc75638eede0d21afda36021c
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-05 23:07:44 +0100

    Feat: Add addTask function with validation

--- server/services/taskService.js
+++ server/services/taskService.js
@@ -1,3 +1,44 @@
 // Describes what a task looks like
 // Task title, time and type
 // To help with using a real database
+
+const tasks = [];
+
+// Function to get all tasks
+function getAllTasks() {
+    return tasks;
+};
+
+// Function to add a new task
+function addTask(task) {
+    // To check for required fields
+    if (!task.title || !task.time || !task.type) {
+        throw new Error("Missing task details: title, time or type");
+    }
+
+    // Create new task with a uniquee id 
+    const newTask = {
+        id: Date.now().toString(), 
+        title: task.title,
+        time: task.time,
+        type: task.type,
+    };
+
+    // Add to tasks array
+    tasks.push(newTask);
+
+    // Return new task
+    return newTask;
+};
+
+// Function to delete a task by ID
+function deleteTask(taskId) {
+
+};
+
+// Export the functions so other files can use them
+module.exports = {
+    getAllTasks,
+    addTask,
+    deleteTask,
+};
