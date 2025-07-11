29aba9fc912d0f3aebc2fdce51ae344227578a94
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-11 23:56:59 +0100

    Feat: Add Calendar page and routing set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import Home from './pages/Home';
 import AddTask from './pages/AddTask';
+import Calendar from './pages/Calendar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/addTask' element={<AddTask />}/>
+        <Route path='/calendar' element={<Calendar />}/>
       </Routes>
     </>
   )
---
+++ client/src/pages/Calendar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Calendar(){
+    return(
+        <div className="calendar-container">
+            <p>TODO: Add Calendar to display tasks in various views. E.g. daily, 3-day, weekly, monthly, yearly</p>
+        </div>
+    )
+}
