367dbde477ef088124d92ce3c5d1e57c22dded36
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-11 23:48:17 +0100

    Feat: Add Home page and routing set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import {Routes, Route} from 'react-router-dom';
 import './App.css'
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home(){
+    return (
+        <div className="home-container">
+            <h2>Let's start planning!</h2>
+        </div>
+    )
+}
