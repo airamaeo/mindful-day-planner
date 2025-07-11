84926cc757ae2cde12866ab4637c6d2166688a3c
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-11 23:54:12 +0100

    Feat: Add AddTask page and routing set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import {Routes, Route} from 'react-router-dom';
 import './App.css'
 
 import Home from './pages/Home';
+import AddTask from './pages/AddTask';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/addTask' element={<AddTask />}/>
       </Routes>
     </>
   )
---
+++ client/src/pages/AddTask.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function AddTask(){
+    return(
+        <div className="addtask-container">
+            <p>TODO: Form for adding tasks</p>
+        </div>
+    )
+}
