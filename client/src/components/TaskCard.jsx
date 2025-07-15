d077393cfe9909502bcbb0ba8d391083645ea93c
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-15 20:50:23 +0100

    Feat: Add TaskCard component and import to Home

---
+++ client/src/components/TaskCard.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function TaskCard(){
+    return(
+        <div className="taskcard-container">
+            <p>TODO: Displays title, time and type of task</p>
+        </div>
+    )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import TaskCard from "../components/TaskCard";
 
 export default function Home(){
     return (
         <div className="home-container">
             <h2>Let's start planning!</h2>
+            <TaskCard />
         </div>
     )
 }
