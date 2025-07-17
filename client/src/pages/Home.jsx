520695c148fed4d96caab3ca2133035f320295e3
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-17 18:53:20 +0100

    Feat: Connect frontend to backend and fetch tasks on Home load

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
 import TaskCard from "../components/TaskCard";
 
 export default function Home(){
+    const [tasks, setTasks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {    
+        setLoading(true);
+
+        const backendUrl = 'http://localhost:5000/api/tasks';
+
+        axios.get(backendUrl)
+            .then((res) => {
+                setTasks(res.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError('Unable to get task');
+                setLoading(false);
+            });
+    }, []);
+
     return (
         <div className="home-container">
             <h2>Let's start planning!</h2>
--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const axios = require('axios');
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
 const taskRoutes = require('./routes/taskRoutes');
 app.use('/api/tasks', taskRoutes);
 
+// Sart server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
