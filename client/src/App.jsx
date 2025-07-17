df61f9c1875d6811e7e5c88821a00512b9bf965c
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-17 20:51:29 +0100

    Fix: Fix routing issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,14 @@ import './App.css'
 import Home from './pages/Home';
 import AddTask from './pages/AddTask';
 import Calendar from './pages/Calendar';
+import Settings from './pages/Settings';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/addTask' element={<AddTask />}/>
+        <Route path='/tasks' element={<AddTask />}/>
         <Route path='/calendar' element={<Calendar />}/>
         <Route path='/settings' element={<Settings />}/>
       </Routes>
