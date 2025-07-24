ed05ddf95d968f9c116287193679de94cc567998
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-07-24 20:24:48 +0100

    Fix: Issues with task detail modal

--- client/src/components/TaskDetailModal.jsx
+++ client/src/components/TaskDetailModal.jsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, {useEffect} from "react"
 
 export default function TaskDetailModal({task, onClose}){    
     if(!task) {
         return null;
     };
 
+    useEffect(() => {
+        const handleKey = (e) => {
+            if (e.key === 'Escape'){
+                onClose();
+            }
+        };
+
+        window.addEventListener('keydown', handleKey);
+        return () => {
+            window.removeEventListener('keydown', handleKey);
+    }}, [onClose]);
+    
     return (
         <div className="taskDetail-overlay" onClick={onClose}>
             <div 
@@ -14,6 +26,7 @@ export default function TaskDetailModal({task, onClose}){
                 <button className="taskDetail-close-btn" onClick={onClose}>
                     X
                 </button>
+                <h2>{task.title}</h2>
                 <p>{task.date}</p>
                 <p>{task.time}</p>
                 <p>{task.type}</p>
